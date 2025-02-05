79bad7428d55eaad63e5224bc60bf15506c61018
AR_Afsun <168043307+AR-Afsun@users.noreply.github.com>
2025-02-05 19:51:03 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ st.title("This is a python website")
 st.subheader("Its created by MD Afsun Rahenam Anuprash.")
 st.write("I created the website for testing purpuse")
 with st.expander("Click to show a video"):
-  st.video("afsun.mp4)
+  st.video("afsun.mp4")
