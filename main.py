ea708d7245e88427f1131783de33970ffb2ff314
AR_Afsun <168043307+AR-Afsun@users.noreply.github.com>
2025-02-05 19:49:01 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ import streamlit as st
 st.title("This is a python website")
 st.subheader("Its created by MD Afsun Rahenam Anuprash.")
 st.write("I created the website for testing purpuse")
+st.video("afsun.mp4)
