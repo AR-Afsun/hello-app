c6375c908eb0c26f93e333d6cffb82f8dfb76b86
AR_Afsun <168043307+AR-Afsun@users.noreply.github.com>
2025-02-05 19:50:34 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ import streamlit as st
 st.title("This is a python website")
 st.subheader("Its created by MD Afsun Rahenam Anuprash.")
 st.write("I created the website for testing purpuse")
-st.video("afsun.mp4)
+with st.expander("Click to show a video"):
+  st.video("afsun.mp4)
